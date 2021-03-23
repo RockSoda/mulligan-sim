cb3a95cabc42cdeb9a715ce1d29f15004a387d71
Sigh <yxh644166727@gmail.com>
2021-03-23 19:50:57 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,207 +1,119 @@
-import { useState } from "react";
+import { useState, useEffect } from "react"
 
-const api = "https://api.scryfall.com/cards/named?exact=";
+const api = "https://api.scryfall.com/cards/named?exact="
 
 function App() {
-  var [query, setQuery] = useState('');
-  const [card, setCard] = useState({});
-  const [card1, setCard1] = useState({});
-  const [card2, setCard2] = useState({});
-  const [card3, setCard3] = useState({});
-  const [card4, setCard4] = useState({});
-  const [card5, setCard5] = useState({});
-  const [card6, setCard6] = useState({});
+  const [query, setQuery] = useState('')
+  const [card, setCard] = useState([])
+  const [hand, setHand] = useState([])
 
+  const handleFocus = (event) => event.target.select()
 
-  const handleFocus = (event) => event.target.select();
-  const toDeck = evt => {
-    if(evt.key === "Enter" || evt.type === "click"){
-      var decklist = [];
-      var list = [];
-      var deckcode = '';
-      var hand = [];
+  useEffect(() => {
+    toSearch(hand)
+  }, [hand])
 
-      deckcode = query;
+  function toDeck(evt) {
+    if (!evt.key === "Enter" && !evt.type === "click") return
 
-      deckcode = deckcode.split("Sideboard")[0];      
+    let decklist = []
+    let deckcode = ''
 
-      deckcode = deckcode.split("Deck")[1];
+    deckcode = query
 
-      decklist = deckcode.split("(");
+    deckcode = deckcode.split("Sideboard")[0]
 
+    deckcode = deckcode.split("Deck")[1]
 
-      console.log(decklist);
-
-      if(deckcode.length>1 && decklist.length===1){
-        decklist = deckcode.split(" ");
-        var num = 0;
-        var tmp = 0;
-        for(var i = 0; i < decklist.length; i++){
-          if(!isNaN(decklist[i])){
-            if(num!=0){
-              for(var j = 0; j < num; j++){
-                list.push(tmp);
-              }
-            }
-            num = decklist[i];
-            tmp = "";
-          }else{
-            tmp += decklist[i] + "%20";
+    let re = /([0-9]+)([a-z|\s,'"-]*)\(/gi
+    let m
+    let i
+    do {
+      m = re.exec(deckcode)
+      if (m) {
+        for (i = 0; i < m[1]; ++i) {
+          decklist.push(m[2])
+        }
+      }
+    } while (m)
+    if (decklist.length === 0) {
+      re = /([0-9]+)([a-z|\s,'"-]*)/gi
+      do {
+        m = re.exec(deckcode)
+        if (m) {
+          for (i = 0; i < m[1]; ++i) {
+            decklist.push(m[2])
           }
         }
+      } while (m)
+    }
 
-      }else{
-        decklist.pop(decklist.length-1);
+    toHand(decklist)
+  }
 
-        for(var i = 0; i < decklist.length; i++){
-          decklist[i] = decklist[i].split(" ");
-        }
-  
-        for(var i = 0; i < decklist.length; i++){
-          var tmp = "";
-          var num = 0;
+  function toHand(decklist) {
+    let tmpHand = []
+    for (let i = 0; i < 7; i++) {
+      const index = Math.floor(Math.random() * decklist.length)
+      tmpHand.push(decklist[index])
+      decklist.splice(index, 1)
+    }
+    setHand(tmpHand)
+  }
 
-          if(i===0){
-            num = decklist[i][1];
-  
-            for(var j = 2; j < decklist[i].length-1; j++){
-              tmp += decklist[i][j] + "%20";
-            }
-  
-          }else{
-            num = decklist[i][2];
-  
-            for(var j = 3; j < decklist[i].length-1 ; j++){
-              tmp += decklist[i][j] + "%20";
-            }
-          }
-          for(var j = 0; j < num; j++){
-            list.push(tmp);
-          }
-  
-        }
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+  }
 
-      }
-      decklist = list;
-      console.log(decklist);
-      for(var i = 0 ; i < 7; i++){
-        const index = Math.floor(Math.random() * decklist.length);
-        hand.push(decklist[index]);
-        decklist.splice(index,1);
-      }
-      
-      for(var i = 0; i < hand.length; i++){
-        query = hand[i];
-        setTimeout(search(i), 50);
-      }
-
+  async function toSearch(hand) {
+    for (let i = 0; i < hand.length; i++) {
+      search(i, hand[i])
+      await sleep(50 * (i + 1))
     }
   }
 
-  const search = i =>{
+  async function search(i, q) {
 
-    fetch(`${api}${query}`)
-    .then(res => res.json())
-    .then(result => {
-      //setQuery('');
-      switch(i){
-        case 0:
-          setCard(result);
-          break;
-        case 1:
-          setCard1(result);
-          break;
-        case 2:
-          setCard2(result);
-          break;
-        case 3:
-          setCard3(result);
-          break;
-        case 4:
-          setCard4(result);
-          break;
-        case 5:
-          setCard5(result);
-          break;
-        case 6:
-          setCard6(result);
-          break;
-        default:
-          break;
-      }
-      //console.log(result);
-    });
-  }
-
-  /*const search = evt => {
-    if(evt.key === "Enter"){
-      fetch(`${api}${query}`)
+    await fetch(`${api}${q}`)
       .then(res => res.json())
       .then(result => {
-        setQuery('');
-        setCard(result);
-        console.log(result);
-      });
-    }
-  }*/
+        if (i === 0) {
+          setCard([result])
+        } else {
+          setCard(prevCards => {
+            return [...prevCards, result]
+          })
+        }
+      })
+  }
+
   return (
     <div className="app">
       <main>
         <div className="search-box">
-          <input 
-           type="text"
-           className="search-bar" 
-           placeholder="Paste your deck code here..." 
-           onChange={e => setQuery(e.target.value)}
-           value={query}
-           onKeyPress={toDeck}
-           onFocus={handleFocus}
-           />
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Paste your deck code here..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={toDeck}
+            onFocus={handleFocus}
+          />
         </div>
         <div className="container">
-          {(typeof card.id != "undefined") ? (
+          {(card.length === 7) && (card.map((c) => (
             <div className="pic">
-              <img src = {(typeof card.image_uris != "undefined") ? (card.image_uris.border_crop) : (card.card_faces[0].image_uris.border_crop)}></img>
+              <img src={(typeof c.image_uris != "undefined") ? (c.image_uris.border_crop) : (c.card_faces[0].image_uris.border_crop)}></img>
             </div>
-          ) : ('')}
-          {(typeof card1.id != "undefined") ? (
-            <div className="pic">
-              <img src = {(typeof card1.image_uris != "undefined") ? (card1.image_uris.border_crop) : (card1.card_faces[0].image_uris.border_crop)}></img>
-            </div>
-          ) : ('')}
-          {(typeof card2.id != "undefined") ? (
-            <div className="pic">
-              <img src = {(typeof card2.image_uris != "undefined") ? (card2.image_uris.border_crop) : (card2.card_faces[0].image_uris.border_crop)}></img>
-            </div>
-          ) : ('')}
-          {(typeof card3.id != "undefined") ? (
-            <div className="pic">
-              <img src = {(typeof card3.image_uris != "undefined") ? (card3.image_uris.border_crop) : (card3.card_faces[0].image_uris.border_crop)}></img>
-            </div>
-          ) : ('')}
-          {(typeof card4.id != "undefined") ? (
-            <div className="pic">
-              <img src = {(typeof card4.image_uris != "undefined") ? (card4.image_uris.border_crop) : (card4.card_faces[0].image_uris.border_crop)}></img>
-            </div>
-          ) : ('')}
-          {(typeof card5.id != "undefined") ? (
-            <div className="pic">
-              <img src = {(typeof card5.image_uris != "undefined") ? (card5.image_uris.border_crop) : (card5.card_faces[0].image_uris.border_crop)}></img>
-            </div>
-          ) : ('')}
-          {(typeof card6.id != "undefined") ? (
-            <div className="pic">
-              <img src = {(typeof card6.image_uris != "undefined") ? (card6.image_uris.border_crop) : (card6.card_faces[0].image_uris.border_crop)}></img>
-            </div>
-          ) : ('')}
-
+          )))}
           <div className="vertical-center">
             <button onClick={toDeck}>Mulligan</button>
           </div>
         </div>
       </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
